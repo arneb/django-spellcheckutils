7483cc9c30ef8afff66e629e15e6a9e3068ca4d8
Arne Brodowski <arne@rcs4u.de>
2008-12-09 15:21:24 +0100

    added a second template-filter which will add suggestions to misspelled words.

--- spellcheckutils/templatetags/spellcheckutils_tags.py
+++ spellcheckutils/templatetags/spellcheckutils_tags.py
@@ -7,6 +7,7 @@ register = Library()
 
 PUNCTUATION = """.,;:!?()'" """ # stripped from words before checking
 HTML_SNIPPET = '<span class="spellingerror">%(word)s</span>' # how to mark misspelled words
+HTML_SNIPPET_SUGGEST = '<span class="spellingerror" title="%(suggestions)s">%(word)s</span>'
 
 
 @register.filter
@@ -31,7 +32,10 @@ def spellcheck(value, args='de'):
         {{ text|spellcheck:"en" }}
     
     """
-    wordlist = value.split()
+    try:
+        wordlist = value.split()
+    except AttributeError:
+        wordlist = []
     speller = get_speller(args)
     for i,word in enumerate(wordlist):
         word = word.strip(PUNCTUATION)
@@ -39,5 +43,33 @@ def spellcheck(value, args='de'):
             wordlist[i] = wordlist[i].replace(word, HTML_SNIPPET % {'word':word})
     return mark_safe(' '.join(wordlist))
 
+
+@register.filter
+def spellcheck_suggest(value, args='de'):
+    """
+    The same as ``spellcheck`` (see above) but adds suggestions to wrong words
+    in a ``title`` attribute, so that javascript can pick them up.
+    
+    NOTE: only use one of the templatetags at a time because ``get_speller()``
+    will only work correctly on the first call. This problem will be addressed 
+    in a later release.
+    
+    """ 
+    try:   
+        wordlist = value.split()
+    except AttributeError:
+        wordlist = []
+    speller = get_speller(args)
+    for i,word in enumerate(wordlist):
+        word = word.strip(PUNCTUATION)
+        if not speller.check(str(word.encode(settings.DEFAULT_CHARSET))):
+            try:
+                s_list = speller.suggest(word)
+                suggestions = [unicode(w, 'utf-8') for w in s_list]
+                wordlist[i] = wordlist[i].replace(word, HTML_SNIPPET_SUGGEST % {'word':word, 'suggestions': ",".join(suggestions)})
+            except:
+                wordlist[i] = wordlist[i].replace(word, HTML_SNIPPET_SUGGEST % {'word':word, 'suggestions': ""})
+    return mark_safe(' '.join(wordlist))
+    
     
     
